eef9eff4b484aaf7d26145a5b9436c9ac9f55a99
wxn <649733108@qq.com>
2019-05-28 16:51:39 +0800

    add new Solutions

---
+++ src/Solution14.java
@@ -0,0 +1,56 @@
+/**
+ * Created by wxn
+ * 2019/5/28 15:29
+ *
+ * 14.最长公共前缀
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ *
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ *
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ *
+ * 所有输入只包含小写字母 a-z 。
+ */
+
+
+public class Solution14 {
+
+	//暴力求解
+	public String longestCommonPrefix(String[] strs) {
+
+		if (strs==null || strs.length==0){
+			return "";
+		}
+
+		StringBuilder sb = new StringBuilder();
+		int index = 0;//记录遍历到字符串的第几个位置
+
+		while (index<strs[0].length()){
+			char c = strs[0].charAt(index);
+			for (int i = 1; i < strs.length; i++) {
+				if (strs[i].length()<index+1 || strs[i].charAt(index)!=c){
+					return sb.toString();
+				}
+			}
+			sb.append(c);
+			index++;
+		}
+		return sb.toString();
+
+	}
+
+	public static void main(String[] args) {
+		String[] strs = {"","aaabbbddd","aaab"};
+		System.out.println(new Solution14().longestCommonPrefix(strs));
+	}
+}
---
+++ src/Solution17.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+/**
+ * Created by wxn
+ * 2019/5/28 15:57
+ *
+ * 17. 电话号码的字母组合
+ *
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ *
+ *
+ *
+ * 示例:
+ *
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 说明:
+ * 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+ *
+ * 2->abc 3->def 4->ghi 5->jkl 6->mno 7->pqrs 8->tuv 9->wxyz
+ */
+
+
+public class Solution17 {
+
+	private ArrayList<String> res;
+
+	//用一个长度为10的桶装每个数字对应的字母
+	private String[] dictionary = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+	//回溯法
+	public List<String> letterCombinations(String digits) {
+
+		res = new ArrayList<>();
+		if (digits.equals("")){
+			return res;
+		}
+
+		findCombination(digits,0,"");
+
+		return res;
+
+
+	}
+
+	private void findCombination(String digits , int index , String s){
+		if (index==digits.length()){
+			res.add(s);
+			return;
+		}
+
+		char c = digits.charAt(index);
+		String letters = dictionary[c-'0'];
+		for (int i = 0; i < letters.length(); i++) {
+			findCombination(digits, index+1, s+letters.charAt(i));
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Solution17().letterCombinations("22"));
+	}
+
+}
