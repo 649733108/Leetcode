c11cd400971bde036a0380fdfea3ad0eaee19a31
WuXinnan <649733108@qq.com>
2019-05-28 21:03:51 +0800

    add new solution

---
+++ src/Solution148.java
@@ -0,0 +1,30 @@
+import utils.linkedlist.ListNode;
+
+/**
+ * Created by wxn
+ * 2019/5/28 21:00
+ *
+ * 148. 排序链表
+ *
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ *
+ * 示例 1:
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2:
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ */
+
+
+public class Solution148 {
+
+	public ListNode sortList(ListNode head) {
+
+		//todo
+
+		return null;
+	}
+}
---
+++ src/Solution82.java
@@ -0,0 +1,61 @@
+import utils.linkedlist.LinkedList;
+import utils.linkedlist.ListNode;
+
+/**
+ * Created by wxn
+ * 2019/5/28 20:09
+ * <p>
+ * 82. 删除排序链表中的重复元素 II
+ * <p>
+ * 给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1->2->3->3->4->4->5
+ * 输出: 1->2->5
+ * 示例 2:
+ * <p>
+ * 输入: 1->1->1->2->3
+ * 输出: 2->3
+ */
+
+
+public class Solution82 {
+
+	public ListNode deleteDuplicates(ListNode head) {
+
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		ListNode dummyHead = new ListNode(0);
+		dummyHead.next = head;
+		ListNode pre = dummyHead;
+		ListNode cur = head;
+		while (cur != null) {
+			while (cur.next != null && cur.next.val != cur.val) {
+				pre = cur;
+				cur = cur.next;
+			}
+			while (cur.next != null && cur.next.val == cur.val) {
+				cur = cur.next;
+			}
+			if (cur!=pre.next){
+				pre.next = cur.next;
+				cur.next = null;
+				cur = pre.next;
+			}else {
+				cur = cur.next;
+			}
+		}
+		return dummyHead.next;
+	}
+
+	public static void main(String args[]) {
+		ListNode head = LinkedList.create(new int[]{1,1,1,1,2,2,3});
+		LinkedList.print(head);
+		ListNode newHead = new Solution82().deleteDuplicates(head);
+		LinkedList.print(newHead);
+	}
+
+}
