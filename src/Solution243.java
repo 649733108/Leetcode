744a516d4772ffe1f57d405be345865ba04658b8
WuXinnan <649733108@qq.com>
2019-06-12 23:28:28 +0800

    add new solution

---
+++ src/Solution243.java
@@ -0,0 +1,32 @@
+import utils.linkedlist.ListNode;
+
+/**
+ * Created by wxn
+ * 2019/6/12 21:31
+ *
+ * 234. 回文链表
+ *
+ * 请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ *
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ *
+ */
+
+
+public class Solution243 {
+
+	public boolean isPalindrome(ListNode head) {
+
+		//todo
+		return false;
+	}
+}
