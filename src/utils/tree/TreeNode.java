69d6b3c095aafc0df10d72e278db34a1a1f2dbcb
wxn <649733108@qq.com>
2019-06-14 16:57:39 +0800

    add new Solutions

---
+++ src/utils/tree/TreeNode.java
@@ -0,0 +1,20 @@
+package utils.tree;
+
+/**
+ * Created by wxn
+ * 2019/6/14 16:25
+ */
+
+
+public class TreeNode {
+
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int val) {
+		this.val = val;
+		left = null;
+		right = null;
+	}
+}
